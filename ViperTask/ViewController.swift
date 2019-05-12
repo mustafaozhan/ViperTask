b831d3411e6c5c978a4c49d43b6d9c79f55adbb5
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-05-12 16:16:41 +0200

    Initial Commit

---
+++ ViperTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperTask
+//
+//  Created by Mustafa Ozhan on 12/05/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
